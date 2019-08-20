ae3a58a2822d98b0fcfaa99e9dc48ceb4971faf5
Apple OSS Distributions <91980991+AppleOSSDistributions@users.noreply.github.com>
2019-08-20 09:57:28 +0000

    file_cmds-287.11.1
    Imported from file_cmds-287.11.1.tar.gz

--- compress/compress.c
+++ compress/compress.c
@@ -54,6 +54,7 @@ __FBSDID("$FreeBSD: src/usr.bin/compress/compress.c,v 1.23 2010/12/11 08:32:16 j
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <locale.h>
 
 #include "zopen.h"
 
@@ -132,8 +133,9 @@ main(int argc, char *argv[])
 		exit (eval);
 	}
 
-	if (cat == 1 && argc > 1)
-		errx(1, "the -c option permits only a single file argument");
+    /*
+     * The UNIX standard requires that `uncompress -c` be able to have multiple file parameters given.
+     */
 
 	for (; *argv; ++argv)
 		switch(style) {
@@ -411,14 +413,23 @@ int
 permission(const char *fname)
 {
 	int ch, first;
+	char resp[] = {'\0', '\0'};
 
 	if (!isatty(fileno(stderr)))
 		return (0);
 	(void)fprintf(stderr, "overwrite %s? ", fname);
+
+	/* Load user specified locale */
+	setlocale(LC_MESSAGES, "");
+
 	first = ch = getchar();
 	while (ch != '\n' && ch != EOF)
 		ch = getchar();
-	return (first == 'y');
+
+	/* only care about first character */
+	resp[0] = first;
+
+	return (rpmatch(resp) == 1);
 }
 
 void
@@ -429,7 +440,7 @@ usage(int iscompress)
 		    "usage: compress [-cfv] [-b bits] [file ...]\n");
 	else
 		(void)fprintf(stderr,
-		    "usage: uncompress [-cfv] [-b bits] [file ...]\n");
+		    "usage: uncompress [-cfv] [file ...]\n");
 	exit(1);
 }
 
